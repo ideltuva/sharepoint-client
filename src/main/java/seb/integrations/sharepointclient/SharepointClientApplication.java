aa5dc84123c8ef08676d603214b1c6898c2db12f
Ignas Deltuva <ideltuva@ideltuvas-MacBook-Pro.local>
2020-08-19 14:04:51 +0300

    project init

---
+++ src/main/java/seb/integrations/sharepointclient/SharepointClientApplication.java
@@ -0,0 +1,13 @@
+package seb.integrations.sharepointclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharepointClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharepointClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seb/integrations/sharepointclient/SharepointClientApplicationTests.java
@@ -0,0 +1,13 @@
+package seb.integrations.sharepointclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharepointClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
